c3911ac0b12d88ebbaae2c87f57f5e2afda8b088
Apprentice <apprentice@dbc-24.local>
2013-11-30 23:39:44 -0800

    stash away this test

--- lib/pwinty/client.rb
+++ lib/pwinty/client.rb
@@ -1,6 +1,8 @@
 require_relative 'request.rb'
 require_relative 'connection.rb'
 require_relative 'error.rb'
+require 'debugger'
+
 module Pwinty
   class Client
 
@@ -10,16 +12,24 @@ module Pwinty
       @target_url = 'https://sandbox.pwinty.com'
     end
 
-    def get_orders
+    def create_order
       begin
-        response = request(:get,'https://sandbox.pwinty.com/v2/Orders',{})
+        response = request1(:get,'https://sandbox.pwinty.com/v2/Orders',{})
         response
       end
     end
-    def get_order(id)
-      response = request(:get,"https://sandbox.pwinty.com/v2/Orders/#{id}",{})
+    def display_order(order_id)
+      begin
+        response = request2(:get,"https://sandbox.pwinty.com/v2/Orders/#{order_id}",{})
+        response
+      end
+    end
+
+    def add_to_order(order_id, img)
+      response = request3(:get,"https://sandbox.pwinty.com/v2/Orders/#{order_id}",{},img)
       response
     end
+
     include Connection
     include Request
 
--- lib/pwinty/request.rb
+++ lib/pwinty/request.rb
@@ -1,17 +1,40 @@
+# Usage: response = request(:get,"https://sandbox.pwinty.com/v2/Orders/#{id}",{})
+
 module Pwinty
   class Client
     module Request
-      def request(action,path,options)
-        headers = { 'X-Pwinty-MerchantId'=> @merchantId,
-                    'X-Pwinty-REST-API-Key' => @apiKey }
-        headers.merge!(options[:headers]) if options[:headers]
-        response = connection.send(action,path) do |request|
-          request.body = options[:body] if options[:body]
-          request.headers = headers unless headers.empty?
-          request.url('',options[:query]) if options[:query]
-        end
-        response.body
+
+      #ToDo:: Refactor the requests back into one method,
+      #       then update the client.rb to match
+
+      def request1(action,path,options)
+        header_1 = "X-Pwinty-MerchantId: #{@merchantId}"
+        header_2 = "X-Pwinty-REST-API-Key: #{@apiKey}"
+        cmd = "curl #{path} -H \"#{header_1}\" -H \"#{header_2}\" -d countryCode=GB -d qualityLevel=Pro -d useTracked=true -d recipientName=Mr%20Jones -d address1=The%20Hollies -d addressTownOrCity=Cardiff -d stateOrCounty=Glamorgan -d postalOrZipCode=CF11%201AX -d payment=InvoiceMe"
+        value = `#{cmd}`
+        # $? will return the status
+        # debugger
+        new_value = value.split(/\"/).join
+        order_id = new_value.match(/:(\d*)/)[1]
+        order_id
       end
+
+      def request2(action,path,options)
+        header_1 = "X-Pwinty-MerchantId: #{@merchantId}"
+        header_2 = "X-Pwinty-REST-API-Key: #{@apiKey}"
+        cmd = "curl #{path} -H \"#{header_1}\" -H \"#{header_2}\""
+        value = `#{cmd}`
+        value
+      end
+
+      def request3(action,path,options,img)
+        header_1 = "X-Pwinty-MerchantId: #{@merchantId}"
+        header_2 = "X-Pwinty-REST-API-Key: #{@apiKey}"
+        cmd = "curl #{path} -H \"#{header_1}\" -H \"#{header_2}\" -d type=8x12 -d url=http%3A%2F%2Fdistilleryimage11.ak.instagram.com%2Fd33aafc8b55d11e2a66b22000a9f09de_7.jpg -d md5Hash=\"79054025255fb1a26e4bc422aef54eb4\" -d copies=1 -d priceToUser=299 -d sizing=Crop"
+        value = `#{cmd}`
+        value
+      end
+
     end
   end
 end
