32d60c9d56d0089de636ba2fbbbcd15d4d05e704
Apprentice <apprentice@dbc-24.local>
2013-11-30 21:15:49 -0800

    use require_relative and update sandbox URL to v2 site version

--- lib/pwinty/client.rb
+++ lib/pwinty/client.rb
@@ -7,17 +7,17 @@ module Pwinty
     def initialize(merchantId,apiKey)
       @merchantId = merchantId
       @apiKey = apiKey
-      @target_url = 'https://api.pwinty.com'
+      @target_url = 'https://sandbox.pwinty.com'
     end
 
     def get_orders
       begin
-        response = request(:get,'https://api.pwinty.com/v2/Orders',{})
+        response = request(:get,'https://sandbox.pwinty.com/v2/Orders',{})
         response
       end
     end
     def get_order(id)
-      response = request(:get,"https://api.pwinty.com/v2/Orders/#{id}",{})
+      response = request(:get,"https://sandbox.pwinty.com/v2/Orders/#{id}",{})
       response
     end
     include Connection
